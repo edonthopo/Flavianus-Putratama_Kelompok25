52aac63c27035dd86a9e9376559c832af20ffeec
FlavianusPutratama <128948529+FlavianusPutratama@users.noreply.github.com>
2023-04-03 22:03:13 +0700

    Add files via upload

---
+++ tugas4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class tugas4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama Anda: ");
+        String nama = input.nextLine();
+
+        System.out.println("Selamat datang, " + nama + "!");
+        System.out.println("Silakan pilih jurusan:");
+        System.out.println("1. Teknik Komputer");
+        System.out.println("2. Arsitektur");
+        System.out.println("3. Teknik Sipil");
+
